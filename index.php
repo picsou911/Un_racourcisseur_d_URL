2f604509aa28542199b00193bafa4a399ddf82a1
Jean-Paul Beaudonnet <jpjp03911@gmail.com>
2022-03-24 11:40:44 +0100

    creation picture + css

--- index.php
+++ index.php
@@ -15,17 +15,22 @@
             <header>
                 <img src="pictures/logo.png" alt="logo" id="logo">
             </header>
-
             <h1>Une url longue ? Raccourcissez_là ?</h1>
             <h2>Largement meilleur et plus court que les autres.</h2>
-
             <form method="post" action="../">
                     <input type="url" name="url" placeholder="Collez un lien à raccourcir">
                     <input type="submit" value="Raccourcir">
             </form>
-                
-           
         </div>
     </section>
+    <section id="brands">
+        <div class="container">
+            <h3>Ces marques nous font confiance</h3>
+            <img src="pictures/1.png" alt="1" class="picture">
+            <img src="pictures/2.png" alt="2" class="picture">
+            <img src="pictures/3.png" alt="3" class="picture">
+            <img src="pictures/4.png" alt="4" class="picture">
+        </div>
+    <section>
     </body>
 </html>
