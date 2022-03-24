69b59906669719cfb087487646048f358e1bcc47
Jean-Paul Beaudonnet <jpjp03911@gmail.com>
2022-03-24 09:55:12 +0100

    premier commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Raccourcisseur d'url express</title>
+        <link rel="stylesheet" type="text/css" href="design/default.css">
+        <link rel="icon" href="pictures/favico.png" type="image/png">
+    </head>
+    <body>
+    
+    <section id="hello">
+        <div class="container">
+            <header>
+                <img src="pictures/logo.png" alt="logo" id="logo">
+            </header>
+
+            <h1>Une url longue ? Raccourcissez_là ?</h1>
+            <h2>Largement meilleur et plus court que les autres.</h2>
+
+            <form method="post" action="../">
+                    <input type="url" name="url" placeholder="Collez un lien à raccourcir">
+                    <input type="submit" value="Raccourcir">
+            </form>
+                
+           
+        </div>
+    </section>
+    </body>
+</html>
